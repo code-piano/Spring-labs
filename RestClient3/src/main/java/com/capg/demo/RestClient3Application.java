8087514444ee474872d80914ab16cc15a6df06e2
Prakhar <Prakhar@DESKTOP-O62PCL9>
2020-04-25 15:16:07 +0530

    Lab3 RestClient

---
+++ RestClient3/src/main/java/com/capg/demo/RestClient3Application.java
@@ -0,0 +1,14 @@
+package com.capg.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class RestClient3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestClient3Application.class, args);
+	}
+
+}
